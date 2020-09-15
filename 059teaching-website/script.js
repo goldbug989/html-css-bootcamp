a2fa666e9a763d70bda22c36b46093499fc0a133
goldbug989 <dyannuzzi@gmail.com>
2020-09-14 19:34:32 -0500

    adds comments to javascript file in teaching website folder

--- 059teaching-website/script.js
+++ 059teaching-website/script.js
@@ -2,6 +2,7 @@
 //change paragraph text on center of page depending on link mouseover
 // the links are     create......become......find
 
+//create link
 document.querySelector('#create-link').addEventListener('mouseover',function(){
   changeDisplay('create');
 });//end of anonymous function
@@ -10,7 +11,7 @@ document.querySelector('#create-link').addEventListener('mouseout',function(){
   defaultText('create');
 });//end ouf create link mouse out anonymous function
 
-
+//become link
 document.querySelector('#become-link').addEventListener('mouseover',function(){
   changeDisplay('become');
 });
@@ -19,7 +20,7 @@ document.querySelector('#become-link').addEventListener('mouseout',function(){
   defaultText('become');
 });//end ouf become link mouse out anonymous function
 
-
+//find link
 document.querySelector('#find-link').addEventListener('mouseover',function(){
   changeDisplay('find');
 });
@@ -28,7 +29,8 @@ document.querySelector('#find-link').addEventListener('mouseout',function(){
   defaultText('find');
 });//end ouf become link mouse out anonymous function
 
-//functions do the lifting
+
+//functions do not repeat yourself
 function changeDisplay(paragraph){
     document.getElementById('default-text').classList.remove('show');
     document.getElementById(paragraph).classList.add('show');
