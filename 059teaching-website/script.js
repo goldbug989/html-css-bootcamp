c8317a26277b3f8271f65414ed949693983b532b
goldbug989 <dyannuzzi@gmail.com>
2020-09-14 19:30:26 -0500

    adds javascript file to teaching website

---
+++ 059teaching-website/script.js
@@ -0,0 +1,40 @@
+
+//change paragraph text on center of page depending on link mouseover
+// the links are     create......become......find
+
+document.querySelector('#create-link').addEventListener('mouseover',function(){
+  changeDisplay('create');
+});//end of anonymous function
+//when mouseover leaves link go back to default
+document.querySelector('#create-link').addEventListener('mouseout',function(){
+  defaultText('create');
+});//end ouf create link mouse out anonymous function
+
+
+document.querySelector('#become-link').addEventListener('mouseover',function(){
+  changeDisplay('become');
+});
+// //when mouseover leaves link go back to default
+document.querySelector('#become-link').addEventListener('mouseout',function(){
+  defaultText('become');
+});//end ouf become link mouse out anonymous function
+
+
+document.querySelector('#find-link').addEventListener('mouseover',function(){
+  changeDisplay('find');
+});
+// //when mouseover leaves link go back to default
+document.querySelector('#find-link').addEventListener('mouseout',function(){
+  defaultText('find');
+});//end ouf become link mouse out anonymous function
+
+//functions do the lifting
+function changeDisplay(paragraph){
+    document.getElementById('default-text').classList.remove('show');
+    document.getElementById(paragraph).classList.add('show');
+}
+//function do not repeat yourself
+function defaultText(paragraph){
+    document.getElementById(paragraph).classList.remove('show');
+    document.getElementById('default-text').classList.add('show');
+}
